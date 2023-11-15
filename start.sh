bf00a135c80c292a853ddaa40a207552d392e05d
akshay_codemonk <akshay@codemonk.in>
2023-11-15 16:40:23 +0530

    fix: start.sh issues

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export MESHROOM_ROOT="$(dirname "$(readlink -f "${BASH_SOURCE[0]}" )" )"
 export PYTHONPATH=$MESHROOM_ROOT:$PYTHONPATH
+export OPENCV_IO_ENABLE_OPENEXR=1
 
 # using existing alicevision release
 #export LD_LIBRARY_PATH=/foo/Meshroom-2023.2.0/aliceVision/lib/
